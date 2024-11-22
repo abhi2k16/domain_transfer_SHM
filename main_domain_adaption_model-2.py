0bf11e55e18c558b2d5e9af1360aa955c1a3878d
Abhijeet Kumar <31257269+abhi2k16@users.noreply.github.com>
2024-11-22 16:55:30 +0530

    Update main_domain_adaption_model-2.py

--- main_domain_adaption_model-2.py
+++ main_domain_adaption_model-2.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import scipy.stats as stats
-
 ############################### Load data #################################
 #%%
 x_fe = pd.read_excel("D:/Vibration_Model_stiffplate/Domain_Adapt/FE_data_105_10_to_425_150.xlsx",header = None) 
@@ -29,21 +28,17 @@ idx7 = np.array(range(91,102))
 idx8 = np.array(range(116,129))
 idx9 = np.array(range(142,154))
 idx10 = np.array(range(169,182))
-
 idx_msd = []
 for i in range(10):
     file = 'idx'+str(i+1)
     file = eval(file)  # eval is a method to convert str to object
     idx_msd.extend(file)
-#%%
-###############################################################################
+#%% ###############################################################################
 x_fe_sym_norm_msd = np.zeros([108,494])
 for i,j in enumerate(idx_msd):
     n = x_fe_sym_norm[j,:]
     x_fe_sym_norm_msd [i,:] = n
-
 x_fe_sym_norm = x_fe_sym_norm
-
 ###############################################################################
 x_undam_fe_sym = x_fe_sym_norm[0:208,232]
 x_undam =[]# np.empty([416,1])
@@ -68,20 +63,17 @@ x_undam_np_sym2_msd = np.zeros([35,len(idx_msd)])
 for i,j in enumerate(idx_msd):
     n = x_undam_np_sym2[:,j]
     x_undam_np_sym2_msd[:,i] = n
-
 x_undam_np_sym2 = x_undam_np_sym2  
 # Plot Data
 for i in range(33):
     plt.plot(x_undam_np_sym2[i+2,:])
 plt.show()
-
 ######################## Normalize Exp data ###################################
 x_undam_np_sym1_norm = np.zeros([35,208])
 for i in range(35):
     for j in range(208):
         n = (x_undam_np_sym1[i,j]-np.min(x_undam_np_sym1[i,:]))/(np.max(x_undam_np_sym1[i,:])-np.min(x_undam_np_sym1[i,:]))
         x_undam_np_sym1_norm[i,j] = n
-
 x_undam_np_sym2_norm = np.zeros([35,208])        
 for i in range(35):
     for j in range(208):
@@ -96,23 +88,19 @@ plt.show()
 # Pandas DataFrame
 column = list(np.arange(1,209))
 undam_x_df = pd.DataFrame(x_undam_np_sym2_norm,columns=column)
-
-# Corelation Matrix
+# CorelationMatrix
 co_rel = undam_x_df.corr()
 plt.imshow(co_rel)
 plt.show()
-
 ########### bootstrapping of all column data in loop ##########################
 undam_bst_data = []   # Empty List
 for j in range(208):
     bst = pd.DataFrame({str(j+1):[undam_x_df.sample(20,replace=True)[j+1].mean() for i in range(0,200)]})
     undam_bst_data.append(bst)
-
 # List to data frame in loop by adding column
 undam_bst_df = pd.DataFrame() #Empty DataFrame
 for i in range(208): 
     undam_bst_df[i+1] = undam_bst_data[i]
-
 ########################## Check data is normal Dist #########################
 from scipy.stats import zscore
 import random
@@ -123,36 +111,30 @@ plt.show()
 # Q-Q plot using by obtainig z-score of data and theoretical standard data generated w/ 0 mean and 1 SD
 z_data = sorted(np.array([random.gauss(0,1) for j in range(200)]))
 z_score = np.array(zscore(undam_bst_df[1]))
-
 fig, ax = plt.subplots(5,5, figsize = (12,12))
 for i in range(5):
      for j in range(5):
          ax[i][j].plot(z_data, sorted(np.array(zscore(undam_bst_df[i*5+j+176]))),'b.',label=str(i*5+j+1))  
          ax[i][j].plot(z_data,z_data,'r-')
 plt.show() 
-
 # Corelationmatrix after bootstrap
 bst_co_rel = undam_bst_df.corr()
 plt.imshow(bst_co_rel)
 plt.show()
-
 ################# Estimete mean and sd of original Exp Data ###################
 undam_x_mean_df = []
 for i in range(208):
     m = undam_x_df[i+1].mean()
     undam_x_mean_df.append(m)
-    
 undam_x_std_df = []
 for i in range(208):
     sd = undam_x_df[i+1].std()
     undam_x_std_df.append(sd)
-
 ##################### estimete mean and sd of bst_df ##########################
 undam_mean_df = []
 for i in range(208):
     m = undam_bst_df[i+1].mean()
     undam_mean_df.append(m)
-    
 undam_std_df = []
 for i in range(208):
     sd = undam_bst_df[i+1].std()
@@ -164,20 +146,17 @@ for i in range(5):
          ax[i][j].hist(undam_bst_df[i*5+j+101],label=str(i*5+j+1))  
          ax[0, 1].set_title(str(i*5+j+1))
 plt.show()  
-
 # Plot Histogram of data using SeaBorn
 fig, axes = plt.subplots(5,5, figsize=(16, 16))
 for col, ax in zip(undam_x_df.columns, axes.flatten()):
     sns.histplot(undam_bst_df, x= col+1,kde = True, legend=ax==axes[0,0], ax=ax)
     #ax.set_title(col+51)
-  
 ################### Generate Random Normal Dristribution data #################
 import random
 undam_norm_df_1000 = []
 for i in range(208):
     norm_d = [random.gauss(undam_mean_df[i],undam_std_df[i]) for j in range(1000)]
     undam_norm_df_1000.append(norm_d)
-
 ################# Plot Generated data #########################################
 # List to np array
 undam_norm_df_np_1000 = np.array(undam_norm_df_1000)
@@ -191,7 +170,6 @@ def add_noise(noise_percent, x):
     r = np.random.normal(0,1,size=(x.shape))
     x_noise = x*(1+(noise_percent/100)*r)
     return x_noise
-    
 # generate the noisy data
 #%%
 undam_fe_n_data = np.zeros([208,1000])
@@ -217,27 +195,22 @@ import copy
 
 device='cuda' if torch.cuda.is_available() else 'cpu'
 print(device)
-# Load Data
-#%%
+#%% # Load Data
 x_fe_tensor = torch.from_numpy(np.transpose(undam_fe_n_data)).reshape(1000,208).float().to(device)
 x_exp_tensor = torch.from_numpy(np.transpose(undam_norm_df_np_1000)).reshape(1000,208).float().to(device)  
 # convert ndarray into torch tensor
 #X_train_tensor = torch.from_numpy(X_train).float().to(device)
-
 # Create class Custom Dataset 
 class Fe_Exp_dataset(Dataset):
     def __init__(self):
         # data loading
         self.fe_data = x_fe_tensor # as feature
         self.exp_data = x_exp_tensor # as target
-        self.n_sample = x_fe_tensor.shape[0]
-        
+        self.n_sample = x_fe_tensor.shape[0] 
     def __getitem__(self, index):
         return self.fe_data[index] , self.exp_data[index]
-    
     def __len__(self):
         return self.n_sample
-
 # Dataset
 train_dataset = Fe_Exp_dataset()
 first_data = train_dataset[0]
@@ -247,8 +220,7 @@ from torch.utils.data import random_split
 data_len = len(train_dataset)
 split = int(data_len*.9)
 X_train_undam,X_test_undam = random_split(train_dataset,[split,data_len-split],generator=torch.Generator().manual_seed(42))
-#%%
-# MODEL STRUCTURE
+#%% # MODEL STRUCTURE
 # model 1
 class Conv1D_DA1(nn.Module):
     def __init__(self):
@@ -289,7 +261,6 @@ class Conv1D_DA1(nn.Module):
         x = x.view(-1, 128, 11)
         x = self.conv1d_decoder(x)
         return x.reshape(-1, 208)
-
     def num_flat_features(self,x):
         size=x.size()[1:] #all dimension except the batch dimension
         num_features=1
@@ -317,7 +288,6 @@ class Conv1D_DA2(nn.Module):
                 nn.Tanh(),
                 nn.Linear(512, 208)
             )
-
     def forward(self, x):
         x = x.reshape(-1,1,208)
         x=self.conv1d_encoder(x)
@@ -357,8 +327,7 @@ AE_model = DNN_DA3().to(device)
 #AE_model = Conv1D_DA2().to(device)
 from torchsummary import summary
 summary(AE_model,(1,208))
-# TRAINING STEP
-#%%
+#%% # TRAINING STEP
 batch_size = 100
 def train(model, num_epochs, batch_size, lr):
     print(str(model.__class__.__name__))
@@ -426,15 +395,12 @@ for model in models:
     torch.save(model,file_model) # Save model structure
     file = 'train_'+str(model.__class__.__name__)+'2.pth' #trained model file 
     torch.save(model.state_dict(),file)
-#%%
-# test AE_model
+#%% # test AE_model
 def test_model(x):
     y_hat=AE_model(x)
     y_hat=y_hat.reshape(208,).detach().numpy()
     return y_hat
-######################## Model testing with dam data ########################
-#%%
-# Load debonding experimental data
+#%% Load debonding experimental data
 dam_050_265 = np.loadtxt("x_dam_265_050_46Hz_sym2_100.csv", delimiter=",")
 dam_100_265 = np.loadtxt("x_dam_265_100_54Hz_sym2_100.csv", delimiter=",")
 dam_070_385 = np.loadtxt("x_dam_385_070_46Hz_sym2_100.csv", delimiter=",")
@@ -503,15 +469,13 @@ for i, model in enumerate(models):
         plt.plot(y_hat,'c-.', label = 'model-3')
     #plt.legend('model-1','model-2','model-3')
 plt.show()
-#%%
-###############################################################################
+#%% ###############################################################################
 # Add noise to FEM damaged data
 FE_data_n_3 = np.zeros([208,494])
 for i in range(494):
     n_data = add_noise(noise_percent = 1,x=x_fe_sym_norm[0:208,i])
     FE_data_n_3[:,i] = n_data
-#%%
-# Store transformed FE data 
+#%% # Store transformed FE data 
 models = [Conv1D_DA1().to(device), Conv1D_DA2().to(device), DNN_DA3().to(device)]
 FE_transformed_data = np.zeros([208,494])
 for i in range(494):
@@ -524,13 +488,11 @@ for i in range(494):
     AE_model = model
     y = AE_model(x)
     FE_transformed_data[:,i] = y.detach().numpy()
-#%%
-###################################### AE_model Comparision ###################################
+#%% ###################################### AE_model Comparision ###################################
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
-
 # damage location and size target value
 # location target zone wise for clasification
 #%%
@@ -567,7 +529,6 @@ for i in range(17):
             for k in range(4):
                 t2 =np.array(j+1)
                 fe_size_zone_t[29*i+5+k+(j-1)*4] = t2
-
 fe_size_zone_t = fe_size_zone_t.reshape(493)
 #%% 
 ################################################################################################
@@ -629,8 +590,8 @@ for j, model in enumerate(models):
     plt.xticks(x_ax)
 plt.show()     
 #%%
-#print(f'Model: {str(model.__class__.__name__)}, test: {split*100}%, SVM train Score: {grid_svm.score(x_trn,y_trn):.4f}')
-#print(f'Model: {str(model.__class__.__name__)}, test: {split*100}% SVM test Score: {grid_svm.score(x_tst,y_tst):.4f}')
+print(f'Model: {str(model.__class__.__name__)}, test: {split*100}%, SVM train Score: {grid_svm.score(x_trn,y_trn):.4f}')
+print(f'Model: {str(model.__class__.__name__)}, test: {split*100}% SVM test Score: {grid_svm.score(x_tst,y_tst):.4f}')
 ###################################################################################
 ############################### Dimension reduction PCA ###########################
 ###################################################################################
@@ -669,8 +630,7 @@ for i in range(494):
     dam_tensor = torch.from_numpy(dam).reshape(1,1,208).float().to(device)
     y = test_model(dam_tensor)
     fe_trans_data[:,i] = y
-#%%
-# Concanate FE and Exp data 
+#%% Concanate FE and Exp data 
 FE_EXP_Data = np.concatenate((undam_norm_df_np_1000[:,0:100]
                               ,fe_trans_data,dam_050_265[:,0:100],dam_100_265[:,0:100],dam_070_385[:,0:100]),axis = 1)
 FE_EXP_Data = np.concatenate((fe_trans_data,dam_050_265[:,0:100],dam_100_265[:,0:100],dam_070_385[:,0:100]),axis = 1)
@@ -693,19 +653,15 @@ plt.xlabel('Principal component index')
 plt.legend(loc='center right')
 plt.tight_layout()
 plt.show()
-
-#%%
-# Normalize PCA data
+#%% Normalize PCA data
 X_train_pca_norm = (X_train_pca - np.min(np.min(X_train_pca)))/(np.max(np.max(X_train_pca))-np.min(np.min(X_train_pca))) 
 #%% 
 FE_tranf_undam = X_train_pca_norm[232,:]
-
 FE_tranf_dam = np.concatenate((X_train_pca_norm[0:232,:],X_train_pca_norm[232:493,:]),axis=0)    
 #EXP_000_000 = X_train_pca_norm[0:100,:]
 EXP_050_265 = X_train_pca_norm[493:593,:]
 EXP_100_265 = X_train_pca_norm[593:693,:]
 EXP_070_385 = X_train_pca_norm[693:793,:]
-#%%
 #%% plot pca componenet
 fig = plt.figure()
 ax = plt.axes(projection ="3d")
@@ -733,7 +689,6 @@ for i,x_pca in enumerate(X_exp_pca):
     for j in range(50):
         n_data = add_noise(noise_percent = 10,x=x_pca)
         exp_pca[i*50+j,:] = n_data
-
 fig = plt.figure(figsize=(7,7))
 ax = plt.axes(projection ="3d")
 n1 = 0
@@ -754,8 +709,7 @@ plt.show()
 x_exp_proj = pca.inverse_transform(exp_pca)
 plt.plot(x_exp_proj[0,:])
 plt.show()
-#%%
-####################### RandomForestClass #####################################
+#%% ####################### RandomForestClass #####################################
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
@@ -803,8 +757,7 @@ plt.xlabel('predicted lavel', fontweight = 'bold',fontsize = 10)
 plt.ylabel('true lavel', fontweight = 'bold', fontsize = 10)
 plt.title('rfc',fontweight = 'bold',fontsize = 10)
 plt.show()
-#%%
-################################### SVM #######################################
+#%% ################################### SVM #######################################
 from sklearn.svm import SVC
 rng = np.random.RandomState(0)
 svm = SVC(random_state=42,probability=True)
@@ -813,7 +766,6 @@ svm = SVC(random_state=42,probability=True)
 param_svm = {'C': [ 1, 10, 100, 1000,10000], 
               'gamma': [1,0.1,0.01,0.001,0.0001],
               'kernel': ['rbf']} 
-  
 grid_svm = GridSearchCV(svm, param_svm, cv=6)
 # fitting the model for grid search
 grid_search_svm=grid_svm.fit(x_trn, y_trn)
@@ -835,8 +787,7 @@ plt.xlabel('predicted lavel', fontweight = 'bold',fontsize = 10)
 plt.ylabel('true lavel', fontweight = 'bold', fontsize = 10)
 plt.title('svm',fontweight = 'bold',fontsize = 10)
 plt.show()
-#%%
-##################### GrandBoostClassifier ####################################
+#%% #################### GrandBoostClassifier ####################################
 from sklearn.ensemble import GradientBoostingClassifier
 gbc = GradientBoostingClassifier()
 param_gbc = {
@@ -867,8 +818,7 @@ plt.show()
 #%%
 plt.hist(grid_gbc.predict(EXP_070_385),range=(1,7))
 plt.show()
-#%%
-##################### AdaBoostClassfier  ######################################
+#%% #################### AdaBoostClassfier  ######################################
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
 adabst = AdaBoostClassifier(base_estimator=DecisionTreeClassifier(),random_state=42)
@@ -895,8 +845,7 @@ plt.xlabel('predicted lavel', fontweight = 'bold',fontsize = 10)
 plt.ylabel('true lavel', fontweight = 'bold', fontsize = 10)
 plt.title('Adaboost',fontweight = 'bold',fontsize = 10)
 plt.show()
-#%%
-############################### knn model #####################################
+#%% ############################## knn model #####################################
 from sklearn.neighbors import KNeighborsClassifier
 knn = KNeighborsClassifier()
 param_knn = {
@@ -924,8 +873,7 @@ plt.xlabel('predicted lavel', fontweight = 'bold',fontsize = 10)
 plt.ylabel('true lavel', fontweight = 'bold', fontsize = 10)
 plt.title('knn',fontweight = 'bold',fontsize = 10)
 plt.show()
-#%%
-##################### Stacked Classifier ######################################
+#%% #################### Stacked Classifier ######################################
 from sklearn.ensemble import StackingClassifier
 from sklearn.linear_model import LogisticRegression
 cls_models = [cls_rf, gbc, adabst, knn]
@@ -972,7 +920,6 @@ vote_clf = VotingClassifier(
     estimators=base_estimator,
     voting='hard')
 vote_clf.fit(x_trn, y_trn)
-
 print(f'Vote train Score: {vote_clf.score(x_trn,y_trn):.4f}')
 print(f'Vote test Score: {vote_clf.score(x_tst,y_tst):.4f}')
 y_vote_pred = vote_clf.predict(x_tst)
@@ -989,8 +936,7 @@ plt.xlabel('predicted lavel', fontweight = 'bold',fontsize = 10)
 plt.ylabel('true lavel', fontweight = 'bold', fontsize = 10)
 plt.title('vote',fontweight = 'bold',fontsize = 10)
 plt.show()
-#%%
-################### Function For ROC Curve estimetion  ########################
+#%% ################## Function For ROC Curve estimetion  ########################
 from sklearn.metrics import roc_curve, auc
 def ROC_Curve_avg(y_true, y_prob_pred, name, color, n_classes):
     clf_name = name
@@ -1032,8 +978,7 @@ def ROC_Curve_avg(y_true, y_prob_pred, name, color, n_classes):
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
     plt.legend(loc = 'lower right')
-#%%
-############### ROC For all clf for performance comparision ###################
+#%% ############## ROC For all clf for performance comparision ###################
 from sklearn.preprocessing import OneHotEncoder
 encoder = OneHotEncoder(handle_unknown='ignore')
 y_true = encoder.fit_transform(y_tst.reshape(-1,1))
